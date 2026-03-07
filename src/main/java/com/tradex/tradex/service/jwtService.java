1b2a63f69a0b5c70eb240d60ca5170433d90321f
Amoghhalemani <amoghhalemani@gmail.com>
2026-03-07 23:45:40 +0530

    added JWT Service

---
+++ src/main/java/com/tradex/tradex/service/jwtService.java
@@ -0,0 +1,44 @@
+package com.tradex.tradex.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class jwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration")
+    private long jwtExpiration;
+
+    public String GenerateToken(String identifier)
+    {
+        return buildToken(new HashMap<>(), identifier, jwtExpiration);
+    }
+    private String buildToken(Map<String ,Object> extraClaims, String identifier, long expiration)
+    {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setId(identifier)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey()
+    {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
